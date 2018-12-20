349e39b3e248a4d5f331ae84cdb3cdc027dfee31
ccdle12 <chris.coverdale24@gmail.com>
2018-12-19 19:04:53 -0600

    simnet/scripts/main: adding .btcd folder and conf file

--- simnet/scripts/main.sh
+++ simnet/scripts/main.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Add a btcd.conf file in .btcd.
+mkdir /root/.btcd
+touch /root/.btcd/btcd.conf
+
 # Run BTCD simnet in the background.
 btcd --txindex --simnet --rpcuser=kek --rpcpass=kek debuglevel=debug &
 
