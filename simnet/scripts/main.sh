77a1f517d159d1f235b4d107283562ab108bcbed
ccdle12 <chris.coverdale24@gmail.com>
2018-12-19 19:17:35 -0600

    simnet/alice: separating start from alices cli command

--- simnet/scripts/main.sh
+++ simnet/scripts/main.sh
@@ -5,7 +5,7 @@ mkdir /root/.btcd
 touch /root/.btcd/btcd.conf
 
 # Run BTCD simnet in the background.
-btcd --txindex --simnet --rpcuser=kek --rpcpass=kek debuglevel=debug &
+/simnet/scripts/btcd.sh &
 
 # Keep the container open.
 tail -f /dev/null
