509f4b131f856b9a16fe38c718328e1ee9611ccb
ccdle12 <chris.coverdale24@gmail.com>
2018-12-21 11:46:05 -0600

    simnet/bob/bob: adding cli script for bob

--- simnet/bob/bob.sh
+++ simnet/bob/bob.sh
@@ -1,14 +1,3 @@
-# Function to start alices node.
-start() {
-    lnd --rpclisten=localhost:10002 --listen=localhost:10012 --datadir=./data --tlscertpath=./tls.cert --tlskeypath=./tls.key  --restlisten=localhost:8002 --alias=bob --debuglevel=debug --externalip=localhost:10012 --tlsextraip=0.0.0.0 --bitcoin.simnet --bitcoin.active --bitcoin.node=btcd --btcd.rpcuser=kek --btcd.rpcpass=kek
+#!/bin/bash
 
-}
-
-# Command line function for alices LND Node.
-cli() {
-    lncli --rpcserver=localhost:10002 --tlscertpath=./tls.cert --macaroonpath=./data/chain/bitcoin/simnet/admin.macaroon $@
-}
-
-cmd="$1"
-shift
-eval "$cmd $@"
+lncli --rpcserver=localhost:10002 --tlscertpath=./tls.cert --macaroonpath=./data/chain/bitcoin/simnet/admin.macaroon $@
