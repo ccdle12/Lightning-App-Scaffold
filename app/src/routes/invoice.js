4100916ff780a2a646b8d747593ba5273e4f36e9
ccdle12 <chris.coverdale24@gmail.com>
2019-01-23 15:28:43 -0500

    app/src/routes/payInvoice: backing up

--- app/src/routes/invoice.js
+++ app/src/routes/invoice.js
@@ -1,9 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const router = express.Router();
 const lnClient = require("../lightning-client/client").Client;
 const utils = require("../utils");
 const QRCode = require("qrcode")
+
+// TEMP - Should be a cached for generated but unpaid.
+// In this demo, to stop generating new addresses, if the invoice is already in
+// the cache, don't generate a new one.
+cache = {};
 
 /**
  * POST /getinvoice will generate an invoice on the LND Node.
@@ -34,13 +39,50 @@ router.post("/", async (_, res) => {
        invoice = `There was an error: ${err}`;
        qrcode = "";
     }
-     
-   return res.render("invoice", {invoice: invoice, qrcode: qrcode});
+    
+    // TEMP
+    cache[invoice] = invoice;
+
+    // Update the view with the generated invoice and qrcode.
+    res.render("invoice", {invoice: invoice, qrcode: qrcode});
+
+    // Create a request to subscribe to any invoices.
+    var request = { 
+      add_index: 0,
+      settle_index: 0,
+    }
+
+    var call = lnClient.subscribeInvoices(request)
+    call.on("data", function(response) {
+      // A response was received from the server.
+      console.log(response["payment_request"]);
+      console.log(invoice);
+      
+      // Parse the response from subscribing to Invoices.
+      recvPaymentReq = response["payment_request"];
+      isInvoiceSettled = response["settled"];
+
+      // Check if the receiving invoice is a generated one and if it has been
+      // paid.
+      if ( recvPaymentReq == cache[recvPaymentReq] && isInvoiceSettled ) {
+        console.log("THEY HAVE MATCHED!!!");
+
+        // IDEA: I can redirect the user to a new page.
+        res.redirect("/payinvoice");
+      }
+
+    });
+    call.on("status", function(status) {
+      // The current status of the stream.
+    });
+    call.on("end", function() {
+      // The server has closed the stream.
+    });
 });
 
 // NOTE: (ccdle12) It seems like generated gRPC functions that are created with
-// callbacks, don't like being used in async/await out of the box. Thats why
-// I've wrapped it in a promise.
+// callbacks, don"t like being used in async/await out of the box. Thats why
+// I"ve wrapped it in a promise.
 function addInvoice(invoiceAmt) {
   return new Promise((resolve, reject) => {
     lnClient.AddInvoice(invoiceAmt, (err, res) => {
